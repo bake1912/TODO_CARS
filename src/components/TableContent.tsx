3d2c3340ec45ed7d15f29287b966875f31e2a33a
bake1912 <102677146+bake1912@users.noreply.github.com>
2023-03-03 17:47:04 +0200

    Update TableContent.tsx

--- src/components/TableContent.tsx
+++ src/components/TableContent.tsx
@@ -40,6 +40,73 @@ export const TableContent = ({
       dataIndex: ColumnsTopOptions.CAR,
       key: ColumnsTopOptions.CAR,
     },
+    {
+      title: ColumnsTopOptions.EDUCATION,
+      dataIndex: ColumnsTopOptions.EDUCATION,
+      key: ColumnsTopOptions.EDUCATION,
+    },
+    {
+      title: ColumnsTopOptions.TELEPHONE,
+      dataIndex: ColumnsTopOptions.TELEPHONE,
+      key: ColumnsTopOptions.TELEPHONE,
+    },
+    {
+      title: ColumnsTopOptions.iSDRIVER,
+      dataIndex: ColumnsTopOptions.iSDRIVER,
+      key: ColumnsTopOptions.iSDRIVER,
+    },
+    {
+      title: ColumnsTopOptions.CHILDRENCOUNT,
+      dataIndex: ColumnsTopOptions.CHILDRENCOUNT,
+      key: ColumnsTopOptions.CHILDRENCOUNT,
+    },
+    {
+      title: ColumnsTopOptions.HOBBY,
+      dataIndex: ColumnsTopOptions.HOBBY,
+      key: ColumnsTopOptions.HOBBY,
+    },
+    {
+      title: ColumnsTopOptions.EMAIL,
+      dataIndex: ColumnsTopOptions.EMAIL,
+      key: ColumnsTopOptions.EMAIL,
+    },
+    {
+      title: ColumnsTopOptions.TYPETRANSPORT,
+      dataIndex: ColumnsTopOptions.TYPETRANSPORT,
+      key: ColumnsTopOptions.TYPETRANSPORT,
+    },
+    {
+      title: ColumnsTopOptions.ILLS,
+      dataIndex: ColumnsTopOptions.ILLS,
+      key: ColumnsTopOptions.ILLS,
+    },
+    {
+      title: ColumnsTopOptions.INTERNETPROVIDER,
+      dataIndex: ColumnsTopOptions.INTERNETPROVIDER,
+      key: ColumnsTopOptions.INTERNETPROVIDER,
+    },
+    {
+      title: ColumnsTopOptions.JOB,
+      dataIndex: ColumnsTopOptions.JOB,
+      key: ColumnsTopOptions.JOB,
+    },
+/*
+/*
+/*
+/*
+/*
+ {
+      title: ColumnsTopOptions.CAR,
+      dataIndex: ColumnsTopOptions.CAR,
+      key: ColumnsTopOptions.CAR,
+    },
+/*
+ {
+      title: ColumnsTopOptions.CAR,
+      dataIndex: ColumnsTopOptions.CAR,
+      key: ColumnsTopOptions.CAR,
+    },
+/*
 /*
     {
       title: 'cars',
@@ -84,7 +151,7 @@ export const TableContent = ({
     <>
       <Table
         columns={columns}
-        pagination={{ pageSize: 3 }}
+        pagination={{ pageSize: 8}}
         dataSource={array}
       />
     </>
